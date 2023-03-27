e989349680b5b31fbbb49ae2d1a045b47c7fd7bc
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-03-27 11:45:36 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "mybucket" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
-    region = "us-east-1"
+    region = "eu-north-1"
   }
 }
 
@@ -88,5 +88,5 @@ resource "kubernetes_cluster_role_binding" "example" {
 
 # Needed to set the default region
 provider "aws" {
-  region  = "us-east-2"
+  region  = "eu-north-1"
 }
