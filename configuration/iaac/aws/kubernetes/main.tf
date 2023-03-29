b1b8976ab5a4b7ba4d89a720d9509aa99e041ee4
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-03-29 14:58:19 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -12,14 +12,7 @@ terraform {
     region = "ap-south-1"
   }
 }
-terraform {
-  required_providers {
-    aws = "~> 4.47.0"
-    random = "~> 3.4.3"
-    tls = "~> 4.0.4"
-    cloudinit = "~> 2.2.0"
-  }
-}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.5.1"
