c97a72737334ba806af187776f1da3c01003ba76
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-03-29 14:33:09 +0530

    Create terraform.tf

---
+++ configuration/iaac/aws/kubernetes/terraform.tf
@@ -0,0 +1,11 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = "~> 4.47.0"
+    random = "~> 3.4.3"
+    tls = "~> 4.0.4"
+    cloudinit = "~> 2.2.0"
+  }
+}
