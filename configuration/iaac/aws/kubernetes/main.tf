cb21dd9d2ca704e3f9bae7a721bfd0849d857ff1
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-03-31 09:37:28 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,89 +1,75 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+# aws --version
+# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
+# Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
+# terraform-backend-state-in28minutes-123
+# AKIA4AHVNOD7OOO6T4KI
 
-provider "aws" {
-  region = "ap-south-1"
-}
 
 terraform {
   backend "s3" {
-    bucket = "terraform-backendstate-in28minutes-123" # Will be overridden from build
+    bucket = "mybucket" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
-    region = "ap-south-1"
+    region = "ap-south-11"
   }
 }
 
+resource "aws_default_vpc" "default" {
+
+}
+
+# data "aws_subnet_ids" "subnets" {
+#   vpc_id = aws_default_vpc.default.id
+# }
+
+
+
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.example.token
- }
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+#   load_config_file       = false
+ # version                = "~> 1.21"
+}
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.5.1"
+# module "my-cluster" {
+#   source          =  "terraform-aws-modules/eks/aws"
+#   cluster_name    = "local.my-cluster"
+#   cluster_version = "1.14"
 
-  cluster_name    = "aws_eks_cluster_via_terraform"
-  cluster_version = "1.24"
+module "my-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster_in_aws_eks"
+#   cluster_version = "1.14"
+  subnet_ids = ["subnet-00c368b9a79c179d6", "subnet-0f570706600928f59" ]
+  #subnets         = ["subnet-01f9ebf3562398329", "subnet-0291156351ccb436b"] #CHANGE
+  #subnets = data.aws_subnet_ids.subnets.ids
+  vpc_id          = aws_default_vpc.default.id
 
-  vpc_id                         = "vpc-084402f29218a055c"
-  subnet_ids                     = ["subnet-00c368b9a79c179d6", "subnet-0f570706600928f59" ]
-  cluster_endpoint_public_access = true
+  #vpc_id         = "vpc-1234556abcdef"
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
+#   node_groups = [
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-
-      instance_types = ["t2.micro"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      instance_type = "t2.micro"
+      max_capacity  = 3
+      desired_capacity = 3
+      min_capacity  = 1
     }
   }
 }
-#   load_config_file       = false
 
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
-# data "aws_iam_policy" "ebs_csi_policy" {
-#   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-# }
-
-# module "irsa-ebs-csi" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-#   version = "4.7.0"
-
-#   create_role                   = true
-#   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-#   provider_url                  = module.eks.oidc_provider
-#   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-#   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-# }
-
-# resource "aws_eks_addon" "ebs-csi" {
-#   cluster_name             = module.eks.cluster_name
-#   addon_name               = "aws-ebs-csi-driver"
-#   addon_version            = "v1.5.2-eksbuild.1"
-#   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-#   tags = {
-#     "eks_addon" = "ebs-csi"
-#     "terraform" = "true"
-#   }
-#     }
-data "aws_eks_cluster" "example" {
-  name =  module.eks.cluster_name 
+data "aws_eks_cluster" "cluster" { 
+  name = "my-cluster_in_aws_eks"
 }
 
-data "aws_eks_cluster_auth" "example" {
-  name =  module.eks.cluster_name
+data "aws_eks_cluster_auth" "cluster" { 
+  name = "my-cluster_in_aws_eks"
 }
 
-
+# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# ServiceAccount needs permissions to create deployments 
+# and services in default namespace
 resource "kubernetes_cluster_role_binding" "example" {
   metadata {
     name = "fabric8-rbac"
@@ -100,4 +86,7 @@ resource "kubernetes_cluster_role_binding" "example" {
   }
 }
 
-  
+# Needed to set the default region
+provider "aws" {
+  region  = "ap-south-1"
+}
